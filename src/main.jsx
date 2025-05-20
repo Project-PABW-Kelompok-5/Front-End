49482a7d8972e844ec466130c6690cf84e53bac6
desssti06 <11221033@student.itk.ac.id>
2025-05-20 17:56:51 +0800

    updt

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,28 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import RegisterPage from './pages/Register.jsx';
-import LoginPage from './pages/Login.jsx';
-import Homepage from './pages/homepage.jsx';
-import ManageUsers from './pages/admin/ManageUsers.jsx';
-import ManageProduct from './pages/admin/ManageProduct.jsx';
-import ManageCourier from './pages/admin/ManageCourier.jsx';
-import Dashboard from './pages/admin/Dashboard.jsx';
-import EcommerceDashboard from './pages/users/DashboardV1.jsx';
-import PrivateRoute from './components/PrivateRoute.jsx';
+import RegisterPage from "./pages/Register.jsx";
+import LoginPage from "./pages/Login.jsx";
+import Homepage from "./pages/homepage.jsx";
+import ManageUsers from "./pages/admin/ManageUsers.jsx";
+import ManageProduct from "./pages/admin/ManageProduct.jsx";
+import ManageCourier from "./pages/admin/ManageCourier.jsx";
+import Dashboard from "./pages/admin/Dashboard.jsx";
+import EcommerceDashboard from "./pages/users/DashboardV1.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Wallet from "./pages/users/wallet.jsx";
 
-import './index.css';
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         {/* Public Routes */}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        
+        <Route path="/wallet" element={<Wallet />} />
 
         {/* Protected User Routes */}
         <Route element={<PrivateRoute role="user" />}>
