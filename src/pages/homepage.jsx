c755e9fa8d07337ad7597ebf256f7be3ad503513
depssss <11221087@student.itk.ac.id>
2025-05-20 23:42:08 +0800

    fix

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -167,7 +167,7 @@ const Homepage = () => {
 
   return (
     <div
-      className="min-h-screen bg-cover bg-center"
+      className="min-h-screen bg-cover bg-center relative"
       style={{ backgroundImage: `url(${Bg})` }}
     >
       <div className="absolute inset-0 bg-black opacity-70"></div>
